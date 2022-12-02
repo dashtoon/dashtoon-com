4b6908961c8f5d3ce2f84e5d8ca8aedf5a457d62
Mohammad Zaryab <mohammadzaryab0@gmail.com>
2022-12-02 11:26:15 +0530

    Change router

--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-
-const routerBaseName = process.env.PUBLIC_URL;
- 
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter basename={routerBaseName}>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
